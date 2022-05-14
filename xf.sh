290df09bef7254144044436d56bf3fb61fc02992
Tonyluk123456 <81285457+Tonyluk123456@users.noreply.github.com>
2022-05-14 14:06:37 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "55c0cee2-8328-4533-b81f-a896c880bcb6",
                         "alterId": 0
                     }
                 ],
